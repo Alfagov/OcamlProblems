f60f05ce190cb78a880725922f72ba5e1f06a315
Alfagov <lorenzopulcini@protonmail.ch>
2023-07-06 23:37:59 +0200

    Run-Length Encoding

--- list_test/_build/default/bin/main.ml
+++ list_test/_build/default/bin/main.ml
@@ -1,9 +1,18 @@
+let run_lenght_enc list =
+  let rec aux count acc = function
+  | [] -> []
+  | [x] -> (count + 1, x) :: acc
+  | h :: (b :: _ as t) -> if h = b then aux (count + 1) acc t
+  else aux 0 ((count + 1, h) :: acc) t in
+  List.rev (aux 0 [] list)
 
-let palindrome l =
-  l = List.rev l
-
+let rec print_tuples = function
+| [] -> ()
+| (a, b) :: rest ->
+  Printf.printf "(%i, %i) " a b;
+  print_tuples rest
 
 let () = 
-let l = [1;2;3;2;1] in 
-  let reversed = palindrome l in
-  print_endline (Bool.to_string reversed)
+let l = [1;1;1;1;5;5;5;4;3;3;7;7] in 
+  let reversed = run_lenght_enc l in
+  print_tuples reversed
