5522e20e6e1f3f9f68a2c643e0ac81fe25e6e6f4
Alfagov <lorenzopulcini@protonmail.ch>
2023-07-06 23:22:29 +0200

    palindrome

--- list_test/bin/main.ml
+++ list_test/bin/main.ml
@@ -1,14 +1,9 @@
 
-
-let reverse list =
-  let rec aux o = function
-  | [] -> o
-  | h :: t ->  aux (h :: o) t
-in
-aux [] list
+let palindrome l =
+  l = List.rev l
 
 
 let () = 
-let l = [1;2;3;4;5] in 
-  let reversed = reverse l in
-  List.iter (Printf.printf "%d ") reversed
+let l = [1;2;3;2;1] in 
+  let reversed = palindrome l in
+  print_endline (Bool.to_string reversed)
